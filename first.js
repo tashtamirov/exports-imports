8a79dea6e53352ab5a14e244a30964989f0a8526
tashtamirov <haron-2000@mail.ru>
2023-01-17 03:03:48 +0300

    создал первый проект с импортами и экспортами

---
+++ first.js
@@ -0,0 +1,6 @@
+const a = 5
+const b = 3
+
+const sum = (a, b) => a + b
+
+export default {firstParametr: a, secondParametr: b, func: sum}
---
+++ second.js
@@ -0,0 +1,3 @@
+import obj from './first.js'
+
+console.log(obj)
